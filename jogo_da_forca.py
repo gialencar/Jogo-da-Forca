162943f4412f0bc10d79eefd69f82f819e46afe1
gialencar <gilson.alencar94@gmail.com>
2020-07-05 19:59:39 -0300

    Refactoring

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,7 +1,18 @@
 from random import randint
 from unicodedata import normalize
 
-# abre um arquivo com as palavras, armazena em uma string e converte para uma lista
+
+def remover_acentos(string):
+    normalizado = normalize('NFD', string)
+    return normalizado.encode('ascii', 'ignore').decode('utf8').lower()
+
+
+def validar_chute(string):
+    # Retorna True se a string tiver 1 caractere
+    return len(string) == 1
+
+
+# abre um arquivo com as palavras, armazena em uma string e converte para uma lista TODO: escolha de nível
 arquivo = open('palavras_faceis.txt', 'r', encoding='UTF-8')
 lista_de_palavras = arquivo.read().split('\n')
 arquivo.close()
@@ -11,40 +22,29 @@ sorteio = randint(0, len(lista_de_palavras))
 # e usa esse número para escolher uma palavra aleatória da lista
 palavra = lista_de_palavras[sorteio]
 parcial = "_" * len(palavra)  # cria uma string de "_" do tamanho da palavra
-tentativas = 0
-letras_usadas = ''
-
-
-def remover_acentos(string):
-    normalizado = normalize('NFD', string)
-    return normalizado.encode('ascii', 'ignore').decode('utf8').lower()
-
-
-def validar_chute(string):
-    tamanho = len(string)
-    return tamanho == 1  # retorna True caso verdadeiro
-
+n_tentativas = 0
+letras_usadas = ""
 
 while True:
     chute = input("-- Entre com uma letra --> ")
     if validar_chute(chute):  # se o chute for válido
-        tentativas += 1
-        letras_usadas += chute + ' '
+        n_tentativas += 1
+        letras_usadas += chute + " "
         palavra_normalizada = remover_acentos(palavra)
-        print("A letra aparece", palavra_normalizada.count(chute), "vezes")
-        print('Tentativas =', tentativas)
+        print(f"A letra aparece {palavra_normalizada.count(chute)} vezes")
+        print(f"Tentativas = {n_tentativas}")
 
-        contagem = 0  # inicia um contador
+        contador = 0
         for letra in palavra:
             if remover_acentos(letra) == chute:
                 parcial_lista = list(parcial)  # converte parcial para lista
-                parcial_lista[contagem] = letra  # substitui a letra na lista
-                parcial = ''.join(parcial_lista)  # converte a lista de volta para string
-            contagem += 1  # adiciona 1 ao contador
+                parcial_lista[contador] = letra  # substitui a letra na lista
+                parcial = "".join(parcial_lista)  # converte a lista de volta para string
+            contador += 1  # adiciona 1 ao contador
 
     print('\n')
     print(parcial)
-    print("Letras já usadas:", letras_usadas)
+    print(f"Letras já usadas: {letras_usadas.upper()}")
 
     if "_" not in parcial:
         print("Game Over\nParabéns!")
